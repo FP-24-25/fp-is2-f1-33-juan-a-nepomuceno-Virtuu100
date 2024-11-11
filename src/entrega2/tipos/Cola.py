7018c8caf8b7acfda2b893c1d2ffed7786b41a64
alvar <alvar@PORTATIL-ALVARO.technicolor>
2024-11-11 19:28:36 +0100

    entrega 2

---
+++ src/entrega2/tipos/Cola.py
@@ -0,0 +1,22 @@
+'''
+Created on 31 oct 2024
+
+@author: alvar
+'''
+# entrega2/tipos/Cola.py
+from __future__ import annotations
+from typing import Generic, TypeVar
+from entrega2.tipos.Agregado_lineal import Agregado_lineal
+
+E = TypeVar('E')
+
+class Cola(Agregado_lineal[E]):
+    @staticmethod
+    def of() -> Cola[E]:
+        return Cola()
+
+    def add(self, e: E) -> None:
+        self._elements.append(e)
+
+    def __str__(self):
+        return f"Cola({self._elements})"
